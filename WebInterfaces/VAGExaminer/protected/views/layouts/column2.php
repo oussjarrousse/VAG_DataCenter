d65725e0e64bfed27fa8f00fadf96bfac7b065f3
leysan12 <lnu@munich-innovation.com>
2014-06-26 14:37:30 +0200

    Some css changes, iPad

--- WebInterfaces/VAGExaminer/protected/views/layouts/column2.php
+++ WebInterfaces/VAGExaminer/protected/views/layouts/column2.php
@@ -1,10 +1,8 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="span-19">
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
-</div>
 <!-- Portlet<div class="span-5">
 	<div id="sidebar">
 	<?php
--- WebInterfaces/VAGExaminer/protected/views/layouts/main.php
+++ WebInterfaces/VAGExaminer/protected/views/layouts/main.php
@@ -12,64 +12,71 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main_sidebar.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
+<div id="wrap">
+	<div id="main">
 
-<div id="page">
+		<div class="header">
+			<h1 class="title"><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+		</div><!-- header -->
 
+		<div class="content">
+
+			<?php if(isset($this->breadcrumbs)):?>
+				<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+				'homeLink'=>CHtml::link('Search Patient', array('/site/index')),
+				'links'=>$this->breadcrumbs,
+				)); ?><!-- breadcrumbs -->
+			<?php endif?>
+
+			<?php echo $content; ?>
+
+			<div class="clear"></div>
+
+			<!--<div id="footer">
+				Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+				All Rights Reserved.<br/>
+				<?php echo Yii::powered(); ?>
+			</div> footer -->
+
+		</div><!-- container -->
+	</div><!-- page -->
+</div><!-- wrap -->
+
+<div id="sidebar">
 	<div class="header">
-		<div class="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
-<div class="container">
-	<div id="sidebar_menu">
+		<p class="title">Menu</p>
+	</div><!--header-->
+			
 	<div class="content">
-	<ul class="nav">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
+		<ul class="nav">
+			<?php $this->widget('zii.widgets.CMenu',array(
+				'items'=>array(
 				array('label'=>'Search Patient', 'url'=>array('/site/index')),
 				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
+				),
+			)); ?>
 			<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-			'htmlOptions'=>array('class'=>'operations'),
-		));
-		$this->endWidget();
-	?>
+				$this->beginWidget('zii.widgets.CPortlet', array(
+				'title'=>'Operations',
+				));
+				$this->widget('zii.widgets.CMenu', array(
+				'items'=>$this->menu,
+				'htmlOptions'=>array('class'=>'operations'),
+				));
+				$this->endWidget();
+			?>
 		</ul>
 	</div>
-	</div><!-- mainmenu -->
-
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'homeLink'=>CHtml::link('Search Patient', array('/site/index')),
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-
-	<!--<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div> footer -->
-
-</div><!-- page -->
-
+</div><!-- sidebar menu -->
 </body>
 </html>
